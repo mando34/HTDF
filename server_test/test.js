62a1fba37f054df797a56256fb8b33f2355fdc27
mando56 <89607986+mando34@users.noreply.github.com>
2023-11-02 15:34:36 -0600

    Added JS & CSS for HTML FILE & server_test folder

---
+++ server_test/test.js
@@ -0,0 +1,5 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    var header = document.querySelector('h1');
+    header.textContent = 'Hello, Web App! (Updated by JavaScript)';
+});
