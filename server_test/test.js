c636c700534495990ab2163a11bb7c6cd0053929
mando56 <89607986+mando34@users.noreply.github.com>
2023-11-30 15:30:36 -0700

    Delete server_test/test.js

--- server_test/test.js
+++
@@ -1,5 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function () {
-    var header = document.querySelector('h1');
-    header.textContent = 'Hello, Web App! (Updated by JavaScript)';
-});
