22a0f9c525d71257ce4276e4d0d4d3eca5b7125b
mando56 <89607986+mando34@users.noreply.github.com>
2023-11-30 15:33:12 -0700

    Add files via upload

--- server_test/static/script.js
+++ server_test/static/script.js
@@ -1,1 +1,26 @@
+// console.log('Hello From JS');
+$(document).ready(function(){
+    $('#uploadBttn').on('click', function(e){
+        e.preventDefault();
+        var file = $('fileInput').val();
 
+        if(file){
+            var formData = new formData();
+            formData.append('file',file);
+
+            fetch('/', {
+                method: 'POST',
+                body: file
+            })
+            .then(response => response.text())
+            .then(message => {
+                console.log(message);
+            })
+            .catch(error => {
+                console.error('Error', error);
+            });
+        } else {
+            console.log('No File Selected.');
+        }
+    });
+});
